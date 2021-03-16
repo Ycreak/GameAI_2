7d0bba8f890b26047bc79f464cdd82bd3a15321d
Unknown <y.creak@hotmail.com>
2021-03-16 15:31:38 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Mine
+Go to folder ./gym_gvgai/envs/gvgai.
+
+Run $ ./run.sh to run the program.
+Edit the Test.java file in ./gym_gvgai/envs/gvgai/src/tracks/singlePlayer/
+
+To change games, edit ./gym_gvgai/envs/gvgai/games.csv.
--- gym_gvgai/envs/gvgai/run.sh
+++ gym_gvgai/envs/gvgai/run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-java -classpath builtclient tracks.singlePlayer.Test
+src_folder='./src'
+build_folder='builtclient'
+rm -rf ${build_folder}
+mkdir -p ${build_folder}
+find ${src_folder} -name "*.java" > sources.txt
+javac -d ${build_folder} @sources.txt
+#run built client
+java -classpath ${build_folder} tracks.singlePlayer.Test
+
